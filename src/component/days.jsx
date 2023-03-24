357f7d6977ad29fd1b92f90d29f7481bfdf38059
Musa Opaluwa <innovate.musa.opaluwa@gmail.com>
2023-03-24 22:45:32 +0100

    fix

--- src/component/days.jsx
+++ src/component/days.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-
+// import { Sunny, Cloudy, Rain, Snow } from 'weather-styled-icon';
+import {Rain} from 'weather-styled-icon';
 const DayOverview = (props) => {
-  const { day, img, Temp } = props;
+  const { day, Temp } = props;
 
   return (
      
-      <div className="  bg-white shadow rounded-[20px] p-[6px] m-[2px] items-center text-black font-semibold">
-        <img  src={img} alt='icon' className='m-auto'/>
-        <h6 className=""> {day}</h6>
+      <div className="  bg-white shadow rounded-[20px] p-[6px] text-center m-[2px] items-center text-black font-semibold">
+        <h1 className="w-[full] m-auto">
+        <Rain  />
+        </h1>
+        <h6 className="text-2xl"> {day}</h6>
         <h4>{Temp}</h4>
         
       </div>
--- src/component/daysCard.jsx
+++ src/component/daysCard.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import DayOverview from "./days";
-import Vector from "../assets/Vector.svg";
 
 const DaysCard = () => {
   return (
     <div className="grid grid-cols-7 mr-[20px] text-center items-center">
-      <DayOverview img={Vector} day="Sun" Temp={23} />
-      <DayOverview img={Vector} day="Mon" Temp={23} />
-      <DayOverview img={Vector} day="Tue" Temp={23} />
-      <DayOverview img={Vector} day="Wed" Temp={23} />
-      <DayOverview img={Vector} day="Thur" Temp={23} />
-      <DayOverview img={Vector} day="Fri" Temp={23} />
-      <DayOverview img={Vector} day="Sat" Temp={23} />
+      <DayOverview  day="Sun" Temp={23} />
+      <DayOverview  day="Mon" Temp={23} />
+      <DayOverview  day="Tue" Temp={23} />
+      <DayOverview  day="Wed" Temp={23} />
+      <DayOverview  day="Thur" Temp={23} />
+      <DayOverview  day="Fri" Temp={23} />
+      <DayOverview  day="Sat" Temp={23} />
     </div>
   );
 };
