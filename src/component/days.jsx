cdcd665e844acbce6bb3c262c47c76746cef3b0b
Musa Opaluwa <innovate.musa.opaluwa@gmail.com>
2023-08-24 19:21:01 +0100

    change from react to next

--- src/component/days.jsx
+++ src/component/days.jsx
@@ -6,7 +6,7 @@ const DayOverview = (props) => {
 
   return (
      
-      <div className="  bg-white shadow rounded-[20px] p-[6px] text-center m-[2px] items-center text-black font-semibold">
+      <div className=" md:bg-[red] lg:bg-white md:w-[fit-content] shadow rounded-[20px] p-[6px] text-center m-[2px] items-center text-black font-semibold">
         <Rain   />
         <h6 className="text-2xl"> {day}</h6>
         <h4>{Temp}</h4>
