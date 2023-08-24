b7e36a7db87ec14d8aecf2f91c51a038c5e2fc61
Musa Opaluwa <innovate.musa.opaluwa@gmail.com>
2023-08-24 19:40:29 +0100

    fix error after changing to next

---
+++ src/pages/index.js
@@ -0,0 +1,35 @@
+/* eslint-disable react/jsx-key */
+// import {useState} from 'react';
+// import axios from 'axios';
+import React from "react";
+import Index from "../component";
+import AirMainComponent from "../component/AirComponent/airMainComponent";
+import Aside from "../component/SideComponent/Aside";
+import WeatherCard from "../component/weather/WeatherCard";
+// import SunriseAndSunset from "../component/weather/SunriseAndSunset";
+
+const Main = () => {
+  return (
+    <div className=" flex gap-[30px] m-[20px]">
+      <div className="w-[70%]  mx-2">
+        <Index />
+        <div className="flex items-center ">
+          <div>
+          <AirMainComponent />
+          <AirMainComponent />
+
+          </div>
+          <div>
+
+          <WeatherCard/>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white p-[20px]">
+          <Aside/>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
