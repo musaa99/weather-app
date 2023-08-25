1b14d8e391f8461fd1123f265de7ba0c8bcc7e6a
Musa Opaluwa <innovate.musa.opaluwa@gmail.com>
2023-08-25 16:14:18 +0100

    fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bacf4421ab7b5b00"; }
+/******/ 		__webpack_require__.h = function() { return "a9b0993bd27b8da7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
